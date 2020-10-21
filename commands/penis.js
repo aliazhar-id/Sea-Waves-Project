44d2364ff9a4806b3957f40d276a7ce673737347
Ahmad Yaqdhan <42838410+Mednoob@users.noreply.github.com>
2020-10-21 13:55:11 +0900

    Penis Command

---
+++ commands/penis.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'penis',
+  cooldown: 3,
+  description: "Penis Command",
+  execute(message, args) {
+    let user = message.mentions.users.first()
+
+    if(!user) user = message.author
+    
+    message.channel.send({embed: {
+      title: `${user.username} Penis`,
+      description: `8${"=".repeat(Math.floor(Math.random() * 18))}D`
+    }})
+  }
+}
